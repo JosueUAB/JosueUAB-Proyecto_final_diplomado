6273636d532d4ef59d7239d51ac457f349abc068
josueUAB <josue.arista@uab.edu.bo>
2025-11-09 01:33:07 -0400

    Frontend: arreglar imports para runtime JSX automático y corregir vite config

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import TaskList from './components/TaskList';
 import CreateTask from './components/CreateTask';
 
 export default function App() {
   return (
-    <div className="container">
+    <div  className="container">
       <h1>Gestión de Tareas</h1>
       <CreateTask />
       <TaskList />
